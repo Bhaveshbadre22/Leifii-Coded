dc1e052b3f61a8a06b915542c2feb78bbd41e269
Bhaveshbadre22 <bhaveshnbadre22@gmail.com>
2024-10-03 10:52:52 +0530

    Second Commit

--- src/sections/Fillar/fillar.jsx
+++ src/sections/Fillar/fillar.jsx
@@ -6,9 +6,9 @@ const Fillar = () => {
     <div>
       <div className="h-[120vh] md:h-[120vh] w-full bg-white overflow-hidden">
         <div className="h-[65%] w-full flex flex-col md:flex-row items-center">
-          <div className="order-2 md:order-1 w-full lg:w-[70%] text-[4rem] md:text-[5rem] lg:text-[10rem] font-questrial mt-[2rem] lg:mt-[8rem] ml-[2rem] lg:ml-[5rem] items-center">
-            <div>CREATIVE</div>
-            <div>DESIGN</div>
+          <div className="order-2 md:order-1 w-full lg:w-[90%] text-[4rem] md:text-[5rem] lg:text-[6rem] font-questrial mt-[2rem] lg:mt-[8rem] ml-[2rem] lg:ml-[5rem] items-center">
+            <div>DESIGNING YOUR</div>
+            <div>LEAVES</div>
           </div>
           <div className="order-1 md:order-2 w-full lg:w-[30%] h-full mt-[2rem] lg:mt-[6rem] lg:pt-[0rem] lg:mr-[8rem] flex items-center justify-center">
             <div className="bg-slate-600 h-[18rem] md:h-[14rem] lg:h-[18rem] w-[18rem] md:w-[14rem] lg:w-[30rem] rounded-full overflow-hidden">
