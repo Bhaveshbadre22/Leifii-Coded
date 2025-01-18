a7831a2068338a855ca77585f865e6eecf795a59
Bhaveshbadre22 <bhaveshnbadre22@gmail.com>
2025-01-19 01:11:23 +0530

    Final Push

--- src/pages/Landing/landing.tsx
+++ src/pages/Landing/landing.tsx
@@ -70,10 +70,10 @@ const Landing = () => {
         </div>
         <Element />
 
-        {/* <div className="bg-black text-white pt-10">
+        <div className="bg-black text-white pt-10">
           <div className="text-[2.5rem] md:text-[40px] lg:text-[4rem] p-4 sm:p-10 pb-0 flex justify-center font-[Questrial]">
             Why us?
-          </div> */}
+          </div>
 
           {/* <div className="flex flex-col sm:flex-row w-full h-screen sm:h-[85vh] justify-center pl-4 sm:pl-10">
             <div className="w-full sm:w-[75%] md:w-[60%] lg:w-[50%]">
@@ -81,12 +81,12 @@ const Landing = () => {
             </div>
           </div> */}
 
-          {/* <div className="flex justify-center pb-0 mb-0">
+          <div className="flex justify-center pb-0 mb-0">
             <Lottie
               animationData={drag}
               className="w-20 h-20 sm:w-24 sm:h-24 md:w-32 md:h-32 lg:w-40 lg:h-40"
             />
-          </div> */}
+          </div>
         </div>
         <div>
           <Testimonials />
