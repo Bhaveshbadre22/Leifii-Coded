222fc997ece0d2d1fdee49d70fe81c524fda8f25
Bhaveshbadre22 <bhaveshnbadre22@gmail.com>
2025-01-19 01:09:07 +0530

    Final Push

--- src/pages/Landing/landing.tsx
+++ src/pages/Landing/landing.tsx
@@ -81,12 +81,12 @@ const Landing = () => {
             </div>
           </div> */}
 
-          <div className="flex justify-center pb-0 mb-0">
+          {/* <div className="flex justify-center pb-0 mb-0">
             <Lottie
               animationData={drag}
               className="w-20 h-20 sm:w-24 sm:h-24 md:w-32 md:h-32 lg:w-40 lg:h-40"
             />
-          </div>
+          </div> */}
         </div>
         <div>
           <Testimonials />
