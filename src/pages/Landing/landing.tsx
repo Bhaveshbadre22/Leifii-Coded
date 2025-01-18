3ff6271e50298f1715de85d21c872d92fdd2507e
Bhaveshbadre22 <bhaveshnbadre22@gmail.com>
2025-01-19 01:15:28 +0530

    lootie remove

--- src/pages/Landing/landing.tsx
+++ src/pages/Landing/landing.tsx
@@ -82,10 +82,10 @@ const Landing = () => {
           </div> */}
 
           <div className="flex justify-center pb-0 mb-0">
-            <Lottie
+            {/* <Lottie
               animationData={drag}
               className="w-20 h-20 sm:w-24 sm:h-24 md:w-32 md:h-32 lg:w-40 lg:h-40"
-            />
+            /> */}
           </div>
         </div>
         <div>
